c925ee1a03f8e62dfa3e814d610c0625ecc0ed85
Jack <44131061+ThePixelated@users.noreply.github.com>
2023-02-18 18:30:10 +0700

    First iteration V.1
    For now, it returns a big random value.
    
    And as you can see, it used a random memory address to get the "random" value.
    I need to do some research about this, is it okay to do this? or there is going to be a risk but for now,
    I just going to continued

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <string>
+
+//std::string hash_converter(long long sums) {
+//}
+
+long long Random(long base_num = 0, long max_num = 10000) {
+	long long return_value = 0;
+
+	int size = 1;
+	unsigned long long sums = 0;
+	unsigned long long* arr = new unsigned long long[size];
+
+	for (int i = 0; i < 100; i++) {
+		//std::cout << arr[i] << std::endl;
+		sums += arr[i];
+		size++;
+	}
+
+	//std::cout << "\n\n" << sums << "\n";
+
+	//if (sums > max_num) {
+	//	int devident = 10;
+
+	//	while ((sums / devident) > max_num) {
+	//		devident *= 10;
+	//		std::cout << "\n" << sums << "\n";
+	//	}
+
+	//	sums -= devident;
+	//	std::cout << "\n" << sums << "\n";
+	//}
+
+	//if (sums > max_num) sums = sums - (max_num - sums);
+	//std::cout << "\n" << sums << "\n";
+
+	return sums;
+}
 
 int main() {
- 
-  return 0;
+
+	std::cout << Random(1, 10);
+
+	return 0;
 }
